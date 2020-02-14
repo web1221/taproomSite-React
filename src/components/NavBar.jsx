ddf45eeb77155613e0023ec70a80bd1fbacc0154
Angela Weber <angelaweber95@hotmail.com>
2020-02-14 14:19:05 -0800

    add a second layer background to nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,12 +4,20 @@ import { Link } from 'react-router-dom';
 function NavBar(){
   const navBarStyle = {
     backgroundColor: "black",
-    color: "white",
-    padding: "40px"
+    padding: "40px",
+    fontSize: "20px"
+  }
+
+  const topBackground = {
+    border: "2px red solid",
+    padding: "15px",
+    backgroundColor: "rgba(252, 15, 3, .5)"
   }
   return (
     <div style={navBarStyle}>
+      <div style={topBackground}>
       <Link to="/">Home</Link> | <Link to="/taplist">On Tap</Link>
+      </div>
     </div>
   );
 }
--- src/components/TapList.jsx
+++ src/components/TapList.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function TapList(){
   return (
     <div>
-
+      
     </div>
   );
 }
