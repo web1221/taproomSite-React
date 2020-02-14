d5ef98afe4b35a2e02a92aa1b458feaf82e67c5b
Angela Weber <angelaweber95@hotmail.com>
2020-02-14 11:38:24 -0800

    add routing, and new components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/Home'
+import NewKeg from './components/NewKeg'
 
-function App() {
+function App(){
   return (
     <div>
+    <NavBar />
+    <Header />
+    <Switch>
+    <Route exact path='/' component={Home} />
+    <Route exact path='/newKeg' component={NewKeg} />
+    </Switch>
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Home(){
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Home;
