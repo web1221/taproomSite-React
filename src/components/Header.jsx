646e17fc272bcddd1fcd8c95014b850c8f01db41
Angela Weber <angelaweber95@hotmail.com>
2020-02-15 10:07:31 -0800

    style header component and add margin on body

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import BarImg from './headerBarImg.jpg'
 function Header(){
+  const headerImgStyle = {
+    width: "100vw",
+    height: "85vh",
+    margin: "10px"
+
+  }
   return (
     <div>
-
+      <img style={headerImgStyle} src={BarImg} alt='picture of taproom'/>
     </div>
   );
 }
