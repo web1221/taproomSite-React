f4b875c2f7b2ef25e82bacc8630ddf7e5e6da32b
Angela Weber <angelaweber95@hotmail.com>
2020-02-15 10:17:48 -0800

    add hipster ipsom to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,16 @@
+
 import React from 'react';
+import Header from './Header';
 
 function Home(){
   return (
     <div>
-
+    <Header />
+    <div>
+    <p>Lorem ipsum dolor amet selfies fam 3 wolf moon aesthetic vaporware plaid skateboard direct trade woke. Crucifix thundercats pour-over yr wayfarers kitsch, offal health goth. Hammock gentrify iPhone, shoreditch shabby chic kitsch pork belly adaptogen copper mug. Try-hard unicorn cornhole next level flexitarian messenger bag. Etsy umami post-ironic typewriter schlitz, master cleanse distillery keytar lo-fi echo park tilde trust fund YOLO fam celiac.
+    Oh. You need a little dummy text for your mockup? How quaint.
+    I bet you’re still using Bootstrap too…</p>
+    </div>
     </div>
   );
 }
