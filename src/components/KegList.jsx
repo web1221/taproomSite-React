770f653438a99f0edfa571609cc7e5efc250712e
Angela Weber <angelaweber95@hotmail.com>
2020-02-16 17:15:23 -0800

    add logos to cards

--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -1,51 +1,67 @@
 import React from 'react';
 import Kegs from './Kegs'
+import BreakSide from './img/breakside.png'
+import FlyingDog from './img/flyingdog.png'
+import Denizens from './img/denizens.png'
+import HeavySeas from './img/heavyseas.png'
+import BaseCamp from './img/basecamp.png'
+import Pelican from './img/pelican.png'
+import DogFish from './img/dogfish.png'
+
 var masterKegList = [
   {
     names: 'WanderLust',
     brewery: 'Breakside',
-    AlcContent: '6.7% ABV'
+    AlcContent: '6.7% ABV',
+    image: BreakSide
   },
   {
     names: 'SouthSide',
     brewery: 'Denizens Brewing Co.',
-    AlcContent: '7.2% ABV'
+    AlcContent: '7.2% ABV',
+    image: Denizens
   },
   {
     names: 'Family Drama',
     brewery: 'Flying Dog',
-    AlcContent: '8.4% ABV'
+    AlcContent: '8.4% ABV',
+    image: FlyingDog
   },
   {
     names: '120 Minute IPA',
     brewery: 'Dog Fish',
-    AlcContent: '15% ABV'
+    AlcContent: '15% ABV',
+    image: DogFish
   },
   {
     names: 'Peg Leg',
     brewery: 'Heavy Seas',
-    AlcContent: '8% ABV'
+    AlcContent: '8% ABV',
+    image: HeavySeas
   },
   {
     names: 'Hazy Rock',
     brewery: 'Pelican',
-    AlcContent: '6.2% ABV'
+    AlcContent: '6.2% ABV',
+    image: Pelican
   },
   {
     names: 'Straight Outta Portland',
     brewery: 'Migration',
-    AlcContent: '7.3% ABV'
+    AlcContent: '7.3% ABV',
+    image: BaseCamp
   },
 ];
 
 function KegList(){
   return (
     <div>
-    <hr/>
     {masterKegList.map((keg, index) =>
       <Kegs names={keg.names}
       brewery={keg.brewery}
-      AlcContent={keg.AlcContent} />
+      AlcContent={keg.AlcContent}
+      image={keg.image}
+      key={index}/>
     )}
     </div>
   );
--- src/components/Kegs.jsx
+++ src/components/Kegs.jsx
@@ -2,19 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Keg(props){
+
+  const imageSize = {
+    height: "140px",
+    width: "150px"
+  }
+
+  const layoutKeg = {
+    display: "inline-block",
+
+  }
   return (
+    <div style={layoutKeg}>
     <div className="flip-card">
       <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <h3>{props.names} </h3>
+            <img style={imageSize} src={props.image} alt='picture of brewery'/>
+          </div>
 
-        <div className="flip-card-front">
-          <h3>{props.names} </h3>
+          <div class="flip-card-back">
+            <p>{props.brewery}</p>
+            <p><em>{props.AlcContent}</em></p>
+          </div>
         </div>
-
-        <div class="flip-card-back">
-          <p>{props.brewery}</p>
-          <p><em>{props.AlcContent}</em></p>
-        </div>
-        
       </div>
     </div>
   );
@@ -23,7 +34,8 @@ function Keg(props){
 Keg.propTypes = {
   brewery: PropTypes.string.isRequired,
   names: PropTypes.string.isRequired,
-  AlcContent: PropTypes.string
+  AlcContent: PropTypes.string,
+  image: PropTypes.string
 };
 
 export default Keg;
