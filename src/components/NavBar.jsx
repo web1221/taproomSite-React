bac9151ef65a9d27e96c68b6cf2745b7158b1a14
Angela Weber <angelaweber95@hotmail.com>
2020-02-16 20:00:55 -0800

    make changes to navBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,38 +3,44 @@ import { Link } from 'react-router-dom';
 
 function NavBar(){
   const navBarStyle = {
-    backgroundColor: "black",
+    backgroundColor: "#f4eed4",
     borderRadius: "10px",
     padding: "10px",
     fontSize: "20px",
     display: "flex",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
 
   }
 
   const homeLink = {
-    backgroundColor: "black",
+    backgroundColor: "#3c7a91",
     border: "solid 1px white",
     borderRadius: "5px",
-    lineHeight: "100px",
-    padding: "30px",
-    marginLeft: "5px",
     textDecoration: "none",
+    justifyContent: "center",
+    padding: "10px"
   }
 
   const barName = {
-    color: "white",
+    color: "#3c7a91",
+    marginBottom: "10px"
+  }
+
+  const linkNav = {
+    display: "flex"
 
   }
 
 
   return (
     <div style={navBarStyle}>
-    <Link style={homeLink} to="/">Home</Link>
-    <Link style={homeLink} to="/taplist">On Tap</Link>
-    <div style={barName}>
-    <h3>WaBeer Inn</h3>
-    </div>
+      <div style={linkNav}>
+      <p><Link style={homeLink} to="/">Home</Link></p>
+      <p><Link style={homeLink} to="/taplist">On Tap</Link></p>
+      </div>
+      <div style={barName}>
+        <h3>Beer Pitch</h3>
+      </div>
     </div>
   );
 }
