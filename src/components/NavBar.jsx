6436bbcc1ac67915d8d231984489f2b8b314fa12
Angela Weber <angelaweber95@hotmail.com>
2020-02-16 18:37:09 -0800

    add styling to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,38 +4,37 @@ import { Link } from 'react-router-dom';
 function NavBar(){
   const navBarStyle = {
     backgroundColor: "black",
-    padding: "20px",
-    fontSize: "20px"
-  }
-
-  const topBackground = {
-    padding: "5px",
-    backgroundColor: "rgba(252, 15, 3, .5)",
+    borderRadius: "10px",
+    padding: "10px",
+    fontSize: "20px",
     display: "flex",
-    justifyContent: "flex-start",
+    justifyContent: "space-between"
+
   }
 
   const homeLink = {
     backgroundColor: "black",
     border: "solid 1px white",
+    borderRadius: "5px",
     lineHeight: "100px",
-    padding: "0px 50px 0px 50px",
-    textDecoration: "none",
-  }
-  const tapLink = {
-    backgroundColor: "black",
-    border: "solid 1px white",
-    lineHeight: "100px",
-    padding: "0px 50px 0px 50px",
+    padding: "30px",
+    marginLeft: "5px",
     textDecoration: "none",
   }
 
+  const barName = {
+    color: "white",
+
+  }
+
+
   return (
     <div style={navBarStyle}>
-      <div style={topBackground}>
-      <Link style={homeLink} to="/">Home</Link>
-      <Link style={tapLink} to="/taplist">On Tap</Link>
-      </div>
+    <Link style={homeLink} to="/">Home</Link>
+    <Link style={homeLink} to="/taplist">On Tap</Link>
+    <div style={barName}>
+    <h3>WaBeer Inn</h3>
+    </div>
     </div>
   );
 }
