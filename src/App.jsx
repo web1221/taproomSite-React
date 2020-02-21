7d4330e49106f5564c0d39f149106c2730099600
Angela Weber <angelaweber95@hotmail.com>
2020-02-21 11:34:04 -0800

    add application state, and proptypes to new keg

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,33 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home'
 import KegList from './components/KegList'
 import NavBar from './components/NavBar'
-import Header from './components/Header'
 import NewKeg from './components/NewKeg'
+import { v4 } from 'uuid'
 
-function App(){
+class App extends React.Components {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      masterKegList: {}
+    }
+    this.handleAddingNewKegToList = this.handleAddingNewKegToList.bind(this)
+  }
+
+  handleAddingNewKegToList(newKeg){
+    let newKegId = v4()
+    let newMasterKegList = Object.assign({}, this.state.masterKegList, {
+      [newKegId]: newKeg
+    });
+    this.setState({masterKegList: newMasterKegList})
+  }
   return (
     <div>
       <NavBar />
       <Switch>
         <Route exact path='/' component={Home} />
-        <Route exact path='/taplist' component={KegList} />
-        <Route exact path='/newkeg' component={NewKeg} />
+        <Route path='/taplist' component={KegList} />
+        <Route path='/newkeg' render={()=> <NewKeg onNewKegCreation={this.handleAddingNewKegToList} /> } />
       </Switch>
     </div>
   );
--- src/components/NewKeg.jsx
+++ src/components/NewKeg.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function NewKeg(){
   return (
     <div>
-
+      
     </div>
   );
 }
